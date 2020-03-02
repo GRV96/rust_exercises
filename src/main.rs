eb7c8bde77cac1e357d8257675f3c51c669798d6
guyllaume.rousseau.1@ens.etstml.ca <guyllaume.rousseau.1@ens.etsmtl.ca>
2020-03-02 15:26:32 -0500

    Leap year function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod functions;
+use functions::is_leap_year;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Leap year test");
+	println!("-800: {}", is_leap_year(-800));
+	println!("-200: {}", is_leap_year(-200));
+	println!("-16: {}", is_leap_year(-16));
+	println!("0: {}", is_leap_year(0));
+	println!("16: {}", is_leap_year(16));
+	println!("200: {}", is_leap_year(200));
+	println!("800: {}", is_leap_year(800));
 }
