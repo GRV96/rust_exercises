b500493263c1e7ab745e20dc4d7bec2e37c01a45
guyllaume.rousseau.1@ens.etstml.ca <guyllaume.rousseau.1@ens.etsmtl.ca>
2020-03-02 17:22:55 -0500

    Greatest common divisor and swapping variables

--- src/functions.rs
+++ src/functions.rs
@@ -1,3 +1,24 @@
+fn euclidian_algorithm(a:u32, b:u32) -> u32 {
+	let r:u32 = a%b;
+	if r == 0 {
+		return b;
+	}
+	return euclidian_algorithm(b, r);
+}
+
+pub fn greatest_common_divisor(a:u32, b:u32) -> u32 {
+	if a < b {
+		euclidian_algorithm(b, a);
+	}
+	return euclidian_algorithm(a, b);
+}
+
 pub fn is_leap_year(year:i32) -> bool {
 	return year%4==0 && year%100!=0 || year%400==0;
+}
+
+pub fn swap_values(val1:&mut i32, val2:&mut i32) {
+	let tmp:i32 = *val1;
+	*val1 = *val2;
+	*val2 = tmp;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod functions;
+use functions::greatest_common_divisor;
 use functions::is_leap_year;
+use functions::swap_values;
 
 fn main() {
     println!("Leap year test");
@@ -10,4 +12,14 @@ fn main() {
 	println!("16: {}", is_leap_year(16));
 	println!("200: {}", is_leap_year(200));
 	println!("800: {}", is_leap_year(800));
+	
+	println!("\nValue swapping");
+	let mut a:i32 = 45;
+	let mut b:i32 = 27;
+	println!("a = {}, b = {}", a, b);
+	swap_values(&mut a, &mut b);
+	println!("a = {}, b = {}", a, b);
+	
+	println!("\nGreatest common divisor");
+	println!("gcd(a, b) = {}", greatest_common_divisor(a as u32, b as u32));
 }
